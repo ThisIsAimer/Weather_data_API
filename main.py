6213a668d424156480ae2fb62ad99f0cfbdc1163
raunak <raunakbiswas05@gmail.com>
2024-12-12 15:00:36 +0530

    Showing all data from a particular station and upgrading showing temp with station and date

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template
+import numpy
 import pandas
 
 application = Flask(__name__)
@@ -10,14 +11,27 @@ stations = stations[["STAID","STANAME                                 "]]
 def home():
     return render_template("home.html", data = stations.to_html())
 
-
-@application.route("/api/v1/<station>/<date>")
-def about(station,date):
+@application.route("/api/v1/<station>")
+def one_station(station):
     filename = "dataAnalysisWithJupyter/data_small/TG_STAID"+str(station).zfill(6)+".txt"
     data = pandas.read_csv(filename,skiprows=20,parse_dates=["    DATE"])
-    temp = data.loc[data["    DATE"] == date] ["   TG"].squeeze() / 10
-    # return str(temperature)
-    return  {"temp": temp,"station": station,"date": date}
+    data["   TG"] = data["   TG"].mask(data["   TG"] == -9999,numpy.nan)
+    data["TEMP"] = data["   TG"] / 10
+    data = data[["    DATE","TEMP"]]
+    return render_template("oneStation.html", data= data.to_html())
+
+
+
+
+@application.route("/api/v1/<station>/<date>")
+def station_date(station,date):
+    filename = "dataAnalysisWithJupyter/data_small/TG_STAID"+str(station).zfill(6)+".txt"
+    data = pandas.read_csv(filename,skiprows=20,parse_dates=["    DATE"])
+    data["    DATE"] = data["    DATE"].mask(data["    DATE"] != date, numpy.nan)
+    data["   TG"] = data["   TG"].mask(data["   TG"] == -9999, numpy.nan)
+    data["TEMP"] = data["   TG"]/10
+    data = data[["    DATE","TEMP"]].dropna()
+    return  render_template("oneStation.html",data= data.to_html())
 
 
 if __name__ == "__main__":
